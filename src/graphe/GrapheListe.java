4b230e1057e44c3e52971fd451b2e37d7f00ded7
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-06-07 09:17:31 +0200

    commit

--- src/graphe/GrapheListe.java
+++ src/graphe/GrapheListe.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class GrapheListe implements Graphe {
     private List<String> ensNom;
-
+    private List<Noeud> ensNoeuds;
 
     @Override
     public List<String> listeNoeuds() {
@@ -26,4 +26,34 @@ public class GrapheListe implements Graphe {
 
         return res;
     }
+
+    /**
+     *
+     * @param depart le nom du noeud de départ
+     * @param destination le nom du noeud d'arrivée
+     * @param cout le cout de l'arc
+     */
+    public void ajouterArc(String depart, String destination, double cout){
+        //on cree un nouveau noeuds de depart et de destination
+        Noeud nDepart = new Noeud(depart);
+        Noeud nDestination = new Noeud(destination);
+
+        //on verifie si le noeud de depart existe deja
+        boolean existeDepart = false;
+        int i = 0;
+        while ((!existeDepart) && (i < ensNoeuds.size())){
+            Noeud n= ensNoeuds.get(i);
+            if (nDepart.equals(n)){
+                nDepart = n;
+                existeDepart = true;
+            }
+            i++;
+        }
+        nDepart.ajouterArc(destination, cout);
+
+    }
+
+
+
+
 }
