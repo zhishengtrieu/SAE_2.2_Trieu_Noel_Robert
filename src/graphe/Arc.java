4c5e23d686194dbd6fc1aa490df99e5c833635ca
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-06-07 08:56:13 +0200

    ajout des getter et setter

--- src/graphe/Arc.java
+++ src/graphe/Arc.java
@@ -11,6 +11,19 @@ public class Arc {
         this.cout = cout>0?cout:0;
     }
 
+    public double getCout() {
+        return cout;
+    }
 
+    public void setCout(double cout) {
+        this.cout = cout;
+    }
 
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
 }
