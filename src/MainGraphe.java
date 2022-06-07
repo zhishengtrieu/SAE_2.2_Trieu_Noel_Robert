c55d582bfe73ade3ce4251c11b95163f665bb430
Alexandre <alexandre.noel4@etu.univ-lorraine.fr>
2022-06-07 09:11:58 +0200

    Ajout MainGraphe de la figure 1

---
+++ src/MainGraphe.java
@@ -0,0 +1,16 @@
+/**
+ * Main qui crée le graphe représenté dans la figure 1
+ */
+public class MainGraphe {
+
+    public static void main(String[] args) {
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("B", "E", 11);
+        g.ajouterArc("C", "A", 19);
+        g.ajouterArc("D", "B", 23);
+        g.ajouterArc("D", "C", 10);
+        g.ajouterArc("E", "D", 43);
+    }
+}
