a0a48003d1127db857d0568775b75cd37e715c4e
Alexandre <alexandre.noel4@etu.univ-lorraine.fr>
2022-06-07 08:54:49 +0200

    Interafce GrapheÃ

--- src/graphe/Graphe.java
+++ src/graphe/Graphe.java
@@ -1,4 +1,19 @@
 package graphe;
 
+import java.util.List;
+
 public interface Graphe {
+
+    /**
+     * Retourne tous les noeuds du graphe avec leur nom
+     * @return les noms des noeuds du graphe
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Retourne la liste des arcs partant du noeud passé en paramètre
+     * @param n le noeud dont on veut connaitre les arcs adjacents
+     * @return la liste des arcs adjacents au noeud
+     */
+    public List<Arc> suivants(String n);
 }
