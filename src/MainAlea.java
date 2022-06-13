bc866a02ea7d04fc931490e66f47bc252d14dbc2
Antonin10800 <anto10800@gmail.com>
2022-06-13 18:15:39 +0200

    Génération aléatoire de graphes et mainAleatoir

---
+++ src/MainAlea.java
@@ -0,0 +1,13 @@
+import graphe.GrapheListe;
+
+public class MainAlea {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5000; i++) {
+            long avant = System.nanoTime();
+            GrapheListe g = GenerationLabyDur.genereralea(i);
+            long apres = System.nanoTime();
+            System.out.println(apres-avant);
+        }
+
+    }
+}
--- src/MainDijkstra.java
+++ src/MainDijkstra.java
@@ -1,10 +1,9 @@
-import Algorithme.BellmanFord;
 import Algorithme.Dijkstra;
 import graphe.GrapheListe;
 
 import java.io.IOException;
 
-public class MainDjikstra {
+public class MainDijkstra {
 
     public static void main(String[] args) throws IOException {
         GrapheListe g2 = new GrapheListe("Graphes/Graphe1.txt");
