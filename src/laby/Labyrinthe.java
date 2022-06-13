cb62afb96dc916d2fa5a749acbcd56e613ada2ad
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-06-13 18:55:23 +0200

    commit Labyrinthe

--- src/laby/Labyrinthe.java
+++ src/laby/Labyrinthe.java
@@ -1,8 +1,11 @@
 package laby;
 
+import graphe.GrapheListe;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * classe labyrinthe. represente un labyrinthe avec
@@ -127,9 +130,9 @@ public class Labyrinthe {
      *
      * @param action une des actions possibles
      */
-    public int[] deplacerPerso(int i, int j,String action) {
+    public int[] deplacerPerso(int i, int j, String action) {
         // case courante
-        int[] courante = {i,j};
+        int[] courante = {i, j};
 
         // calcule case suivante
         int[] suivante = getSuivant(courante[0], courante[1], action);
@@ -176,6 +179,7 @@ public class Labyrinthe {
 
     /**
      * return mur en (i,j)
+     *
      * @param x
      * @param y
      * @return
@@ -184,4 +188,49 @@ public class Labyrinthe {
         // utilise le tableau de boolean
         return this.murs[x][y];
     }
+
+    public GrapheListe genererGraphe() {
+        GrapheListe g = new GrapheListe();
+        for (int i = 0; i < getLength(); i++) {
+            for (int j = 0; j < getLengthY(); j++) {
+                for (int[] voisin : voisinsValides(i, j)) {
+                    g.ajouterArc("(" + i + "," + j + ")", "(" + voisin[0] + "," + voisin[1] + ")", 1);
+                }
+            }
+        }
+
+
+        return g;
+    }
+
+    /**
+     * Methode qui donne les voisins valides de x,y
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public ArrayList<int[]> voisinsValides(int x, int y) {
+
+        ArrayList<int[]> res = new ArrayList<int[]>();
+
+        //on recupere les positions des voisins
+        res.add(getSuivant(x, y, GAUCHE));
+        res.add(getSuivant(x, y, DROITE));
+        res.add(getSuivant(x, y, HAUT));
+        res.add(getSuivant(x, y, BAS));
+
+        for (int[] pos : res) {
+            //si la position est hors du labyrinthe ou que c'est un mur on la supprime de la liste
+            if (pos[0] < 0 || pos[1] < 0 || pos[0] >= getLength() || pos[1] >= getLengthY()) {
+                res.add(pos);
+            } else if (this.murs[pos[0]][pos[1]]) {
+                res.add(pos);
+            }
+        }
+
+        return res;
+    }
+
+
 }
