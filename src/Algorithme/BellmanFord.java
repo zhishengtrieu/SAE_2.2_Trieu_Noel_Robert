234db6af98632c2a6ac761efaf5a3e53ce66c75a
Antonin10800 <anto10800@gmail.com>
2022-06-13 12:30:34 +0200

    Initialisation BellManFord

--- src/Algorithme/BellmanFord.java
+++ src/Algorithme/BellmanFord.java
@@ -1,11 +1,34 @@
 package Algorithme;
 
+
+import graphe.Arc;
 import graphe.Graphe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BellmanFord implements Algorithme{
 
     public Valeur resoudre(Graphe g, String depart){
+        Valeur res = new Valeur();
 
-        return null;
+        /* Initialisation */
+
+        for(int i =0; i<g.listeNoeuds().size(); i++){
+            res.setValeur(g.listeNoeuds().get(i),Double.MAX_VALUE);
+            res.setParent(g.listeNoeuds().get(i),null);
+            if(g.listeNoeuds().get(i).equals(depart)){
+                res.setValeur(g.listeNoeuds().get(i), 0);
+            }
+        }
+
+        /* Algorithm */
+        /* Chaques étape va être de la taille de tous les noeuds */
+        for(int i =0; i<g.listeNoeuds().size(); i++) {
+            
+
+        }
+
+        return res;
     }
 }
