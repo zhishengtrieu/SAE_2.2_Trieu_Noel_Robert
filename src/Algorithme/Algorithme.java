74913f92779955f5d90d22075b142fc0cc48ecca
Zhi-Sheng <zhishengtrieu@gmail.com>
2022-06-13 12:41:46 +0200

    Optimisation du code

--- src/Algorithme/Algorithme.java
+++ src/Algorithme/Algorithme.java
@@ -2,8 +2,22 @@ package Algorithme;
 
 import graphe.Graphe;
 
+import java.util.ArrayList;
+
 public interface Algorithme {
 
     Valeur resoudre(Graphe g, String depart);
+
+    static Valeur initialiser(Graphe g, String depart){
+        Valeur res = new Valeur();
+        ArrayList<String> Q = new ArrayList<String>(g.listeNoeuds());
+
+        for(String v : Q){
+            res.setValeur(v,Double.MAX_VALUE);
+            res.setParent(v,null);
+        }
+        res.setValeur(depart, 0);
+        return  res;
+    }
 
 }
