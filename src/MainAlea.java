4ef7a22b3cf94c6ee6492d6f78ac81b42f9bca85
Antonin10800 <anto10800@gmail.com>
2022-06-14 11:29:48 +0200

    Test/chrono génération aléatoire de graphe

--- src/MainAlea.java
+++ src/MainAlea.java
@@ -1,18 +1,30 @@
+import Algorithme.BellmanFord;
+import Algorithme.Dijkstra;
+import Algorithme.Valeur;
 import graphe.GrapheListe;
 
 public class MainAlea {
-    public static void main(String[] args) {/*
-        for (int i = 0; i < 5000; i++) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            GrapheListe g = GenerationLabyDur.genererAlea(i);
+            Dijkstra dj = new Dijkstra();
+            BellmanFord bf = new BellmanFord();
             long avant = System.nanoTime();
-            GrapheListe g = GenerationLabyDur.genereralea(i);
+            Valeur v1 = bf.resoudre(g,"1");
             long apres = System.nanoTime();
-            System.out.println(apres-avant);*/
 
-        GrapheListe g = GenerationLabyDur.genereralea(15);
-        System.out.println(g.toGraphViz());
+            long avant2 = System.nanoTime();
+            Valeur v2 = dj.resoudre(g,"1");
+            long apres2 = System.nanoTime();
+
+
+
+            System.out.println((apres - avant)-(apres2 - avant2));
+       /* GrapheListe g = GenerationLabyDur.genererAlea(20000);
+        System.out.println(g.toGraphViz());*/
+        }
+
     }
 
+
 }
-
-
-
